94a38280999718f4920c11215bf717dc34924beb
Matea Lencek <ml6723@student.uni-lj.si>
2016-05-07 14:04:23 +0000

    Zaključek dela na III. nalogi

--- eracun.js
+++ eracun.js
@@ -133,7 +133,11 @@ var pesmiIzRacuna = function(racunId, callback) {
     Track.TrackId IN (SELECT InvoiceLine.TrackId FROM InvoiceLine, Invoice \
     WHERE InvoiceLine.InvoiceId = Invoice.InvoiceId AND Invoice.InvoiceId = " + racunId + ")",
     function(napaka, vrstice) {
-      console.log(vrstice);
+      if(napaka || vrstice.length == 0) {
+        callback(false);
+      } else {
+        callback(vrstice);
+      }
     })
 }
 
@@ -142,13 +146,30 @@ var strankaIzRacuna = function(racunId, callback) {
     pb.all("SELECT Customer.* FROM Customer, Invoice \
             WHERE Customer.CustomerId = Invoice.CustomerId AND Invoice.InvoiceId = " + racunId,
     function(napaka, vrstice) {
-      console.log(vrstice);
+      if(napaka || vrstice.length == 0) {
+        callback(false);
+      } else {
+        callback(vrstice[0]);
+      }
     })
 }
 
 // Izpis računa v HTML predstavitvi na podlagi podatkov iz baze
 streznik.post('/izpisiRacunBaza', function(zahteva, odgovor) {
-  odgovor.end();
+  var form = new formidable.IncomingForm();
+  
+  form.parse(zahteva, function(napaka, polja, datoteke) {
+    strankaIzRacuna(polja.seznamRacunov, function(stranka) {
+      pesmiIzRacuna(polja.seznamRacunov, function(pesmi) {
+        odgovor.setHeader('content-type', 'text/xml');
+        odgovor.render('eslog', {
+          vizualiziraj: true,
+          postavkeRacuna: pesmi,
+          stranka: stranka
+        });
+      });
+    });
+  });
 })
 
 // Izpis računa v HTML predstavitvi ali izvorni XML obliki
